aa64e0cfbd9d8a49970004f798ffd1d1fcad2fcd
Vladimir <tchaikovski@mail.ru>
2021-03-05 14:02:19 +0300

    Добавили формы из файла fields.py

--- educa/settings.py
+++ educa/settings.py
@@ -31,9 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'material.admin',
-    'material.admin.default',
-    # 'django.contrib.admin',
+    # 'material.admin',
+    # 'material.admin.default',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
